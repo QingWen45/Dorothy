68e7070b80b6fc4c0d751cf807e7386c2b730a12
4qw5 <HurrSea@outlook.com>
2020-12-07 13:01:26 +0800

    提交

---
+++ bot.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+# @File : 
+# @Time : 
+# @Author : QingWen
+# @E-mail : hurrsea@outlook.com
+
+import nonebot
+
+# You can pass some keyword args config to init function
+nonebot.init(_env_file=".env.dev")
+app = nonebot.get_asgi()
+
+nonebot.load_builtin_plugins()
+nonebot.load_plugins("src/plugins")
+nonebot.load_plugin("nonebot_plugin_docs")
+nonebot.load_plugin("nonebot_plugin_test")
+nonebot.load_plugin("nonebot_plugin_status")
+
+if __name__ == "__main__":
+    nonebot.run(app="bot:app")
