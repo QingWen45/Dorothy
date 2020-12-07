ff592ca54a6398d0c8d177237f23e416e13667c9
4qw5 <HurrSea@outlook.com>
2020-12-07 20:10:51 +0800

    提交

--- bot.py
+++ bot.py
@@ -6,16 +6,29 @@
 # @Author : QingWen
 # @E-mail : hurrsea@outlook.com
 
+
+from src.utils.yaml_loader import yml_loader
+from pathlib import Path
+
 import nonebot
 
-nonebot.init(_env_file=".env.dev")
+CONFIG_FILE = Path("./configs.yml")
+config = yml_loader(CONFIG_FILE)
+bot_config = config["bot"]
+
+nonebot.init(debug=bot_config["DEBUG"],
+             superusers=set(bot_config["SUPERUSERS"]),
+             nickname=set(bot_config["NICKNAME"]),
+             command_start=set(bot_config["COMMAND_START"]),
+             command_sep=set(bot_config["COMMAND_SEP"]))
 app = nonebot.get_asgi()
 
 nonebot.load_builtin_plugins()
 nonebot.load_plugins("src/plugins")
+
 nonebot.load_plugin("nonebot_plugin_docs")
 nonebot.load_plugin("nonebot_plugin_test")
 nonebot.load_plugin("nonebot_plugin_status")
 
 if __name__ == "__main__":
-    nonebot.run(app="bot:app")
+    nonebot.run(app="bot:app", host=bot_config["HOST"], port=config["PORT"])
---
+++ src/utils/utils/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+# @File : __init__.py.py
+# @Time : 2020/12/7 20:06
+# @Author : QingWen
+# @E-mail : hurrsea@outlook.com
+
+def counter(l: list, elm) -> int:
+    """
+    检查元素出现次数
+    """
+    sum = 0
+    for i in l:
+        if i == elm:
+            sum += 1
+    return sum
